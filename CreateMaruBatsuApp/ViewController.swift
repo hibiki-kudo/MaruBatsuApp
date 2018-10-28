9d5fd568897d34e6404923c2ed185e396778b8a5
hibikikkk <kyou9712@gmail.com>
2018-10-28 22:26:08 +0900

    first commit

---
+++ CreateMaruBatsuApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CreateMaruBatsuApp
+//
+//  Created by 工藤 響 on 2018/10/28.
+//  Copyright © 2018 工藤 響. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
